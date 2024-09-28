dcf6a9cd705debb5416316b90597ff2599b7e15b
Danilo Teixeira <danilo.rt@outlook.com>
2024-09-28 04:27:16 -0300

    feat: create logic to simulate navigation tracker

--- src/app/core/components/calculator/calculator.component.ts
+++ src/app/core/components/calculator/calculator.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 
-
 @Component({
   selector: 'app-calculator',
   standalone: true,
@@ -9,5 +8,12 @@ import { Component } from '@angular/core';
   styleUrl: './calculator.component.scss'
 })
 export class CalculatorComponent {
+  public selectedNavigationItem = 2;
+  public readonly navigationTrackerItems = Array.from({length: 8})
+
+  // Simulates the navigation tracker
+  public onSelectedItem(index: number) {
+  this.selectedNavigationItem = index;
+  }
 
 }
