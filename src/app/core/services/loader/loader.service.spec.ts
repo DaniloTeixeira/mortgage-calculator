0bd7e330b96ab228d7ca9beb627580113ae49033
Danilo Teixeira <danilo.rt@outlook.com>
2024-10-02 02:47:22 -0300

    test: test creation for loader.service

--- src/app/core/services/loader/loader.service.spec.ts
+++ src/app/core/services/loader/loader.service.spec.ts
@@ -1,6 +1,5 @@
 import { TestBed } from '@angular/core/testing';
-
-import { LoaderService } from './loader.service';
+import { LoaderService } from './loader.service'; // ajuste o caminho conforme necessário
 
 describe('LoaderService', () => {
   let service: LoaderService;
@@ -12,5 +11,27 @@ describe('LoaderService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should return false when loading is not set', () => {
+    expect(service.getIsLoading()()).toBe(false); 
+  });
+
+  it('should set loading state to true', () => {
+    service.setIsLoading(true);
+    expect(service.getIsLoading()()).toBe(true); 
+  });
+
+  it('should set loading state to false', () => {
+    service.setIsLoading(false);
+    expect(service.getIsLoading()()).toBe(false); 
+  });
+
+  it('should change loading state reactively', () => {
+    service.setIsLoading(true);
+    expect(service.getIsLoading()()).toBe(true); 
+    
+    service.setIsLoading(false);
+    expect(service.getIsLoading()()).toBe(false); 
   });
 });
