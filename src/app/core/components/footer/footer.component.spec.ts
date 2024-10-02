efcf4071a992a8141e7db6d5527460a75031d32a
Danilo Teixeira <danilo.rt@outlook.com>
2024-10-02 09:47:37 -0300

    test: test creation for footer.component

--- src/app/core/components/footer/footer.component.spec.ts
+++ src/app/core/components/footer/footer.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { By } from '@angular/platform-browser';
 import { FooterComponent } from './footer.component';
 
 describe('FooterComponent', () => {
@@ -20,4 +21,25 @@ describe('FooterComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  
+  it('should have a "Go Back" button with the correct text and classes', () => {
+    const goBackButton = fixture.debugElement.query(By.css('.btn-neutral'));
+    expect(goBackButton).toBeTruthy(); // Verifica se o botão existe
+    expect(goBackButton.nativeElement.classList).toContain('btn');
+    expect(goBackButton.nativeElement.classList).toContain('btn-neutral');
+    expect(goBackButton.nativeElement.classList).toContain('w-[117px]');
+    expect(goBackButton.nativeElement.classList).toContain('h-[52px]');
+    expect(goBackButton.nativeElement.classList).toContain('text-lg');
+  });
+
+  it('should have a "Next Step" button with the correct text and classes', () => {
+    const nextStepButton = fixture.debugElement.query(By.css('.btn-primary'));
+    expect(nextStepButton).toBeTruthy(); // Verifica se o botão existe
+    expect(nextStepButton.nativeElement.classList).toContain('btn');
+    expect(nextStepButton.nativeElement.classList).toContain('btn-primary');
+    expect(nextStepButton.nativeElement.classList).toContain('w-[129px]');
+    expect(nextStepButton.nativeElement.classList).toContain('h-[52px]');
+    expect(nextStepButton.nativeElement.classList).toContain('text-lg');
+  });
+
 });
